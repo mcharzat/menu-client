dfa0ca9d5006a2ef5db2af0a0795622a8d7a3fd9
Maxime_Charzat <23charzatcav@gmail.com>
2022-02-08 18:07:08 +0100

    First structure

--- src/main/java/com/cicdlectures/menuclient/controller/MenuCommandLine.java
+++ src/main/java/com/cicdlectures/menuclient/controller/MenuCommandLine.java
@@ -11,7 +11,21 @@ import java.util.concurrent.Callable;
         description = "Interact with menu server")
 public class MenuCommandLine implements Callable<Integer> {
 
+  @Option(names = "--server-url", description = "Server called.")
+  private String server;
+
+  @Parameters(index = "0", description = "Command to run")
+  private String command;
+
+  @Option(names = "-id", description = "Id of the menu")
+  private int idMenu;
+
   public Integer call() throws Exception {
+    if (command.equals("list-menus")) {
+
+    } else if (command.equals("delete-menu")) {
+
+    }
     return null;
   }
 
