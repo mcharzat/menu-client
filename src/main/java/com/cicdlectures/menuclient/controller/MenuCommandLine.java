f255bb4d18b98317efa210e824c5c6534b769133
Maxime_Charzat <23charzatcav@gmail.com>
2022-02-08 19:27:03 +0100

    First implementation of get and delete fonctionnalities : Http request

--- src/main/java/com/cicdlectures/menuclient/controller/MenuCommandLine.java
+++ src/main/java/com/cicdlectures/menuclient/controller/MenuCommandLine.java
@@ -1,10 +1,17 @@
 package com.cicdlectures.menuclient.controller;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Map;
 import java.util.concurrent.Callable;
 
 @Command(name = "menucli", mixinStandardHelpOptions = true, version = "menucli 1.0",
@@ -12,7 +19,7 @@ import java.util.concurrent.Callable;
 public class MenuCommandLine implements Callable<Integer> {
 
   @Option(names = "--server-url", description = "Server called.")
-  private String server;
+  private String server = "https://menusserver.herokuapp.com";
 
   @Parameters(index = "0", description = "Command to run")
   private String command;
@@ -21,10 +28,41 @@ public class MenuCommandLine implements Callable<Integer> {
   private int idMenu;
 
   public Integer call() throws Exception {
-    if (command.equals("list-menus")) {
+    System.out.println("Test");
+    if (this.command.equals("list_menus")) {
+      // create a client
+      var client = HttpClient.newHttpClient();
 
-    } else if (command.equals("delete-menu")) {
+      // create a request
+      var request = HttpRequest.newBuilder(
+                      URI.create(this.server + "/menus"))
+              .GET()
+              .header("accept", "application/json")
+              .build();
 
+      // use the client to send the request
+      var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+      // the response:
+      System.out.println(response.body());
+      JSONArray array = new JSONArray(response.body());
+      JSONObject json = array.getJSONObject(0);
+      System.out.println(json.getString("name"));
+    } else if (command.equals("delete_menu")) {
+      // create a client
+      var client = HttpClient.newHttpClient();
+
+      // create a request
+      var request = HttpRequest.newBuilder(
+                      URI.create(this.server + "/menus/" + this.idMenu))
+              .DELETE()
+              .build();
+
+      // use the client to send the request
+      var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+      // the response:
+      System.out.println(response.body());
     }
     return null;
   }
