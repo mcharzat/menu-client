4c4bce981cefef8484971d59508a1f09e3646963
Maxime_Charzat <23charzatcav@gmail.com>
2022-02-10 19:00:39 +0100

    Fix and adapt tests

--- src/test/java/com/cicdlectures/menuclient/MenuCommandLineTests.java
+++ src/test/java/com/cicdlectures/menuclient/MenuCommandLineTests.java
@@ -5,6 +5,8 @@ import com.cicdlectures.menuclient.controller.MenuCommandLine;
 import org.junit.jupiter.api.*;
 import picocli.CommandLine;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
@@ -15,15 +17,30 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MenuCommandLineTests {
 
     CommandLine cmd;
-    StringWriter sw;
+
+    final PrintStream originalOut = System.out;
+    final PrintStream originalErr = System.err;
+    final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    final ByteArrayOutputStream err = new ByteArrayOutputStream();
 
     @BeforeAll
     public void init() {
         MenuCommandLine app = new MenuCommandLine();
         this.cmd = new CommandLine(app);
+    }
 
-        this.sw = new StringWriter();
-        this.cmd.setOut(new PrintWriter(this.sw));
+    @BeforeEach
+    public void setUpStreams() {
+        out.reset();
+        err.reset();
+        System.setOut(new PrintStream(out));
+        System.setErr(new PrintStream(err));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
     }
 
     @Test
@@ -32,10 +49,10 @@ public class MenuCommandLineTests {
     public void createNewMenu() {
         int exitCode = cmd.execute("create_menu");
         assertEquals(0, exitCode);
-        assertEquals("[{\"id\":1,\"name\":\"Menu spécial du " +
+        assertEquals("{\"id\":1,\"name\":\"Menu spécial du " +
                 "chef\",\"dishes\":[{\"id\":1,\"name\":\"Bananes aux " +
-                "fraises\"},{\"id\":2,\"name\":\"Bananes flambées\"}]}]" +
-                "\nCreation complete", sw.toString());
+                "fraises\"},{\"id\":2,\"name\":\"Bananes flambées\"}]}" +
+                "\nCreation complete\n", out.toString());
     }
 
     @Test
@@ -47,7 +64,7 @@ public class MenuCommandLineTests {
         assertEquals("[{\"id\":1,\"name\":\"Menu spécial du " +
                 "chef\",\"dishes\":[{\"id\":1,\"name\":\"Bananes aux " +
                 "fraises\"},{\"id\":2,\"name\":\"Bananes flambées\"}]}]" +
-                "\nMenu spécial du chef", sw.toString());
+                "\nMenu spécial du chef\n", out.toString());
     }
 
     @Test
@@ -56,15 +73,15 @@ public class MenuCommandLineTests {
     public void deleteMenu() {
         int exitCode = cmd.execute("delete_menu", "-id=1");
         assertEquals(0, exitCode);
-        assertEquals("\nDelete complete", sw.toString());
+        assertEquals("\nDelete complete\n", out.toString());
     }
 
     @Test
     @Order(4)
     @DisplayName("get all menus with custom url")
     public void getMenusUrl() {
-        int exitCode = cmd.execute("--server-url='https://menusserver.herokuapp.com' list_menus");
+        int exitCode = cmd.execute("--server-url=https://menusserver.herokuapp.com", "list_menus");
         assertEquals(0, exitCode);
-        assertEquals("[]", sw.toString());
+        assertEquals("[]\nNo menus in server.\n", out.toString());
     }
 }
